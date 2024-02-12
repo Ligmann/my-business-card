853262a8fb587e89cfc9209aa5f8b335b9701cb6
Damian Ligmann <dligmann@MacBook-Pro-dligmann.local>
2024-02-12 22:37:32 +0100

    raw version

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,26 +2,42 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `landing-page`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: [{
-    resolve: 'gatsby-source-wordpress',
-    options: {
-      "url": "https://damian.ligmann.dev/graphql"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sass", "gatsby-plugin-google-gtag", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `landing-page`,
+        siteUrl: `https://ligmann.dev`,
     },
-    __key: "images"
-  }]
+    plugins: [
+        {
+            resolve: 'gatsby-source-wordpress',
+            options: {
+                "url": "https://ligmann.dev/graphql",
+                "useACF": true,
+            }
+        },
+        "gatsby-plugin-image",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        "gatsby-plugin-sass",
+        "gatsby-plugin-fontawesome-css",
+        {
+            resolve: "gatsby-plugin-google-gtag",
+            options: {
+                trackingIds: [],
+            },
+        },
+        "gatsby-plugin-sitemap",
+        {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                "icon": "src/images/icon.png"
+            }
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                "name": "images",
+                "path": "./src/images/"
+            },
+            __key: "images"
+        }]
 };
